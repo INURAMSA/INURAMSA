e35bfa03b5ef027d6d696ec300d98a35d1f770a5
M Ismail NuraAmsa <80862172+INURAMSA@users.noreply.github.com>
2021-03-20 19:11:55 +0700

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,14 @@
+#! / usr / bin / bash
+
+pembaruan apt-get
+peningkatan apt-get
+apt-get install nodejs
+apt-get install libwebp
+apt-get install mc
+apt-get install ffmpeg
+apt-get install wget
+apt-get install tesseract
+wget -O ~ /../usr/share/tessdata/ind.traineddata " https://github.com/tesseract-ocr/tessdata/blob/master/ind.traineddata?raw=true "
+npm install
+
+echo  " [*] Semua dependensi telah diinstal, jalankan perintah \" npm start \ " untuk segera memulai skrip "
